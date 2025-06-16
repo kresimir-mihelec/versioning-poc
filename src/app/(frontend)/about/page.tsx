07b0e7bb600bfe1927b0608c7b799b480625aa84
kresimir-mihelec <121818873+kresimir-mihelec@users.noreply.github.com>
2025-06-16 10:52:18 +0200

    feat: Update about us
    * update about
    
    * update metadata

--- src/app/(frontend)/about/page.tsx
+++ src/app/(frontend)/about/page.tsx
@@ -1,20 +1,8 @@
-import { headers as getHeaders } from 'next/headers.js'
 import Image from 'next/image'
-import { getPayload } from 'payload'
 import React from 'react'
-import { fileURLToPath } from 'url'
-
-import config from '@/payload.config'
 import './styles.css'
 
 export default async function HomePage() {
-  const headers = await getHeaders()
-  const payloadConfig = await config
-  const payload = await getPayload({ config: payloadConfig })
-  const { user } = await payload.auth({ headers })
-
-  const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
-
   const appVersion = process.env.APP_VERSION
 
   return (
@@ -29,17 +17,8 @@ export default async function HomePage() {
             width={65}
           />
         </picture>
-        {!user && <h1>Welcome to your new project.</h1>}
-        {user && <h1>Welcome back, {user.email}</h1>}
+
         <div className="links">
-          <a
-            className="admin"
-            href={payloadConfig.routes.admin}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Go to admin panel
-          </a>
           <a
             className="docs"
             href="https://payloadcms.com/docs"
@@ -52,9 +31,6 @@ export default async function HomePage() {
       </div>
       <div className="footer">
         <p>Update this page by editing</p>
-        <a className="codeLink" href={fileURL}>
-          <code>app/(frontend)/page.tsx</code>
-        </a>
         <p>Version: {appVersion}</p>
       </div>
     </div>

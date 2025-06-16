79576f73433f8c6d579ff111b98249d98a8fe456
kresimir-mihelec <121818873+kresimir-mihelec@users.noreply.github.com>
2025-06-16 11:30:13 +0200

    feat: Add contact us page
    Additional description about feature that should be added below the title

---
+++ src/app/(frontend)/contact/page.tsx
@@ -0,0 +1,42 @@
+import Image from 'next/image'
+import React from 'react'
+import './styles.css'
+
+export default async function HomePage() {
+  const appVersion = process.env.APP_VERSION
+
+  return (
+    <div className="home">
+      <div className="content">
+        <picture>
+          <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
+          <Image
+            alt="Payload Logo"
+            height={65}
+            src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg"
+            width={65}
+          />
+        </picture>
+
+        <div className="links">
+          <a
+            className="docs"
+            href="https://payloadcms.com/docs"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Documentation
+          </a>
+        </div>
+      </div>
+      <div className="footer">
+        <p>Update this page by editing</p>
+        <p>Version: {appVersion}</p>
+      </div>
+      <div className="footer">
+        <p>Whatever</p>
+        <p>Version: {appVersion}</p>
+      </div>
+    </div>
+  )
+}
